efa366b7866ffa0a745858ff8c6aa23f2f27db95
Giovannidjuric <giovanni_djuric@hotmail.nl>
2023-11-14 16:38:01 +0100

    Added logic to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('this my js commit');
+//I haven't done any imports yet
+let myNumber = 5;
+if(typeof myNumber == 'number'){
+    console.log('this is a number yeeh!')
+    return true;
+} else {
+    console.log('this is not a number');
+    return false;
+}
