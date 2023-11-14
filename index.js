b57c63c9c15fa34bf0f251299e71038bad60cb74
Giovannidjuric <giovanni_djuric@hotmail.nl>
2023-11-14 16:48:26 +0100

    what is happening?

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('this my js commit');
 //I haven't done any imports yet
 let myNumber = 5;
+let string = 'wtf is happing';
 if(typeof myNumber == 'number'){
     console.log('this is a number yeeh!')
     return true;
